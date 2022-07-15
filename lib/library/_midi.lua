e5ed0afe69b2b754190fee7e365e22beb201fd61
fourhoarder <77514244+fourhoarder@users.noreply.github.com>
2022-07-15 14:24:52 -0700

    Update _midi.lua

--- lib/library/_midi.lua
+++ lib/library/_midi.lua
@@ -9,7 +9,7 @@ local midi_out = function(self, x, y)
   local octave = util.clamp(self:listen(self.x + 2, self.y) or 4, 0, 8)
   local vel = util.clamp(self:listen(self.x + 4, self.y) or 10, 0, 16)
   local length = self:listen(self.x + 5, self.y) or 1
-  local note = self:glyph_at(self.x + 3, self.y) or "C"
+  local note = self:glyph_at(self.x + 3, self.y) or ""
   local transposed = self:transpose(note, octave)
   local n, oct, velocity = transposed[1], transposed[4], math.floor((vel / 16 ) * 127)
 
